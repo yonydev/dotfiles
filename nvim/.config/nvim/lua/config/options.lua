88c3f2a4c428a63973bdcf7d9ff7e0fe0a96ae2f
Yonatan Rojas <yonatanrojas09@gmail.com>
2026-08-15 13:21:41 -0600

    Add neovim: LazyVim setup with omarchy theme hot-reload

---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,16 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+require('config.remote_clipboard').setup()
+vim.opt.relativenumber = true
+vim.g.autoformat = false
+
+local options = {
+  scrolloff = 12,
+  sidescrolloff = 8,
+}
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+
---
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -0,0 +1,1 @@
+/home/yonatan/.config/omarchy/current/theme/neovim.lua
